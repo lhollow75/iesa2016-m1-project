bbcf7e766d074e8b512423f8a48f5c017cbb67d0
juliensallei <magna@live.fr>
2016-06-22 16:40:18 +0200

    #6 Adding info class

---
+++ app/Paris Games Week/Paris Games Week/info.swift
@@ -0,0 +1,17 @@
+//
+//  info.swift
+//  Paris Games Week
+//
+//  Created by Julien Sallei on 22/06/2016.
+//  Copyright © 2016 Paris Games Week. All rights reserved.
+//
+
+import Foundation
+
+class Info {
+    var horaires : String = ""
+    var adresses : String = ""
+    var numeros : String = ""
+    var mails : String = ""
+    var itinéraire : String = ""
+}
