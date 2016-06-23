2385863af73d1b79031be7a811a9c419b13f7b11
Mastere 1 IT <mastere1it@spotlight.sto-002.saint-augustin.in.iesanetwork.com>
2016-06-23 17:10:14 +0200

    Session view added with details #release-1

--- app/Paris Games Week/Paris Games Week/SessionView.swift
+++ app/Paris Games Week/Paris Games Week/SessionView.swift
@@ -40,16 +40,6 @@ class SessionView: UITableViewController {
         return mySessionStore.session.count
     }
 
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
         cell.textLabel!.text = mySessionStore.session[indexPath.row].name
